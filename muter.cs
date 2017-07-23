e9a7b88c434a53583b692292accdaa1da5233412
NotAzebu <30387838+NotAzebu@users.noreply.github.com>
2017-07-24 00:46:42 +0900

    Add files via upload

--- muter.cs
+++ muter.cs
@@ -1,22 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class muter : MonoBehaviour
 {
     //public AudioListener myAudioListener;
-
+    //public Button muteButton;
 
     void Start()
     {
         //myAudioListener = GetComponent<AudioListener>();
         
+        
     }
 
 
-    void Update()
+    void onMouseDown()
     {
         //if (Input.GetKeyUp(KeyCode.Space))
         //{
+            
             AudioListener.volume = 0.0f;
         //}
     }
